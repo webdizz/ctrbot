67a8f64297e54d8149896d1f3374e8fd3f691fbf
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-12-24 20:06:02 +0200

    Now message event can be handled

--- webhook.js
+++ webhook.js
@@ -1,13 +1,39 @@
+'use strict'
+
 const
     express = require('express'),
     router = express.Router();
 
 router.get('*', function (req, res) {
-    res.send('hello get');
+    let VERIFY_TOKEN = process.env.VERIFY_TOKEN || 'TOKEN';
+    console.log('token', VERIFY_TOKEN, 'requested', req.query)
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.debug('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
 });
 
 router.post('*', function (req, res, next) {
-    res.send('hello post');
+    let body = req.body;
+
+    console.log('event', body.entry)
+    if (body.object === 'page') {
+        body.entry.forEach(function (element) {
+            let webhookEvent = element.messaging[0];
+            console.log('messaging payload', webhookEvent, 'text', webhookEvent.message.text);
+        });
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 module.exports = router;
